9c386546e0c1b8b14b4e5866c9181a839a76a931
cheehongtan <96945310+cheehongtan@users.noreply.github.com>
2022-01-03 13:05:47 +0800

    Add files via upload

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿using Calculator.BusinessLogic;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dataSource;
+            dataSource = "( 11.5 + 15.4 ) + 10.1";
+            double result = CalculateBiz.Calculate(dataSource);
+        }
+    }
+}
